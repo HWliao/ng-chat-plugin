e41503588861737bb28be195a4d70f69fd3c6f22
HWliao <499606788@qq.com>
2018-04-05 17:20:30 +0800

    chore: 基础项目划分

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule
+    };
+  }
+}
